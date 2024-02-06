5bd78f416eb1ffd62a598edbcd2b77d443b24bd3
Mohammad Iqbal <ki925053@gmail.com>
2024-02-07 00:14:58 +0530

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
-import Footer from "./footer/footer"
-import Header from "./header/Header"
+import Footer from "./footer/Footer.jsx"
+import Header from "./header/Header.jsx"
 
 
 
